ad685732de2510474a353483b45d7fb1a569515d
Sarah Leong <sleong20@stuy.edu>
2021-01-12 13:04:09 -0500

    added nth

--- Radix.java
+++ Radix.java
@@ -1,13 +1,18 @@
 public class Radix {
     public static int nth(int n, int col) {
-
+        int temp = (int)Math.pow(10, col);
+        return (n % (temp*10) - n%temp)/temp;
     }
 
-    public static int length(int n) {
+    // public static int length(int n) {
 
-    }
+    // }
 
-    public static void merge(MyLinkedList orginal, MyLinkedList[] buckets) {
+    // public static void merge(MyLinkedList orginal, MyLinkedList[] buckets) {
 
+    // }
+
+    public static void main(String[] args) {
+        System.out.println(nth(1234567890,3));
     }
 }
