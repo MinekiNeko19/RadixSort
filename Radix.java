5ba8f93c9511936d46037d0a71f0168c3fb638ef
Sarah Leong <sleong20@stuy.edu>
2021-01-12 13:14:45 -0500

    added length

--- Radix.java
+++ Radix.java
@@ -4,15 +4,16 @@ public class Radix {
         return (n % (temp*10) - n%temp)/temp;
     }
 
-    // public static int length(int n) {
-
-    // }
+    public static int length(int n) {
+        return String.valueOf(n).length();
+    }
 
     // public static void merge(MyLinkedList orginal, MyLinkedList[] buckets) {
 
     // }
 
     public static void main(String[] args) {
-        System.out.println(nth(1234567890,3));
+        System.out.println(nth(1234567890,15));
+        System.out.println(length(1234567890));
     }
 }
