c73f0ef1c752e21dc4ddb69f786d24b4941c5255
Sarah Leong <sleong20@stuy.edu>
2021-01-12 20:23:32 -0500

    did part 4; changed mylinkedlist parameters to sortablelinkedlist parameters

--- Radix.java
+++ Radix.java
@@ -8,7 +8,7 @@ public class Radix {
         return (int)(Math.log10(n) + 1);
     }
 
-    public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
         for (int i = 0; i < buckets.length; i++) {
             original.extend(buckets[i]);
         }
@@ -25,16 +25,16 @@ public class Radix {
         // System.out.println(length(1234567890));
 
         // testing merge
-        MyLinkedList[] b = new MyLinkedList[10];
-        for (int i = 0; i < b.length; i++) {
-            b[i] = new MyLinkedList();
-        }
-        MyLinkedList a = new MyLinkedList();
-        b[5].add("50"); b[4].add("41"); b[1].add("11"); b[1].add("12"); b[4].add("42");
-        b[3].add("32"); b[3].add("32"); b[2].add("23"); b[3].add("34"); b[4].add("44");
-        b[3].add("34"); b[8].add("87"); b[7].add("77"); b[5].add("58"); b[0].add("8");
+        // MyLinkedList[] b = new MyLinkedList[10];
+        // for (int i = 0; i < b.length; i++) {
+        //     b[i] = new MyLinkedList();
+        // }
+        // MyLinkedList a = new MyLinkedList();
+        // b[5].add("50"); b[4].add("41"); b[1].add("11"); b[1].add("12"); b[4].add("42");
+        // b[3].add("32"); b[3].add("32"); b[2].add("23"); b[3].add("34"); b[4].add("44");
+        // b[3].add("34"); b[8].add("87"); b[7].add("77"); b[5].add("58"); b[0].add("8");
 
-        merge(a,b);
+        // merge(a,b);
         // System.out.println(a.toString());
         // bucketPrint(b);
     }
